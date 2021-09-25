e1a7c7e034d505636d16115ff3aa4f0ef8560e3e
Terje Nesthus <terje@ekstremedia.no>
2021-09-25 05:21:58 +0200

    Added longexpo

--- capture.js
+++ capture.js
@@ -1,1 +1,19 @@
-const first = 2
+const PiCamera = require("pi-camera");
+const webServer = '/var/www/html/'
+
+const myCamera = new PiCamera({
+    mode: "photo",
+    output: `${webServer}/test.jpg`,
+    width: 640,
+    height: 480,
+    nopreview: true,
+});
+
+myCamera
+    .snap()
+    .then((result) => {
+        // Your picture was captured
+    })
+    .catch((error) => {
+        // Handle your error
+    });
---
+++ longexpo.sh
@@ -0,0 +1,1 @@
+raspistill -n -o test.jpg -q 90 -ISO 800 -ss 6000000 -awb off -awbg 1.45,1.55 --nopreview
